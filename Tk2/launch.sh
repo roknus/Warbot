94e920fcb1fcd650c07baf5868e945cf5aabb813
roknus <roknus@gmail.com>
2013-11-20 15:46:00 +0100

    a

--- Tk2/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#do more write less :)
-repCourant=`pwd`
-libPath="$repCourant"/lib
-binPath="$repCourant"/bin
-
-java -Djava.library.path=$libPath -Dfile.encoding=Cp1252 -cp "lib/*:.":$binPath madkit.boot.Madkit madkit.kernel.Booter --graphics --config turtlekit2.cfg
